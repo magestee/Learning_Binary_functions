06373cf203ebf5a9d350e7517615234342c761e2
sam <51410648+magestee@users.noreply.github.com>
2024-06-21 17:22:36 +0330

    make fibonaci numbers as output made

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-   let size = 2;
+   let size = 4;
 
    let outputs = generate_all_binary_inputs(size);
 
@@ -15,7 +15,7 @@ fn main() {
    // let trainingset = genrate_the_trainingset(outputs);
    //genrate_the_trainingset(outputs)
    
-   let even = make_odd_num_vec(outputs);
+   let even = make_fibonacci_num_vec(outputs);
 
 
    for (key, value) in &even {
@@ -65,6 +65,16 @@ fn genrate_the_trainingset(n: Vec<Vec<i32>>) -> Vec<TrainingSet>{
     }
 }
 */
+
+fn is_fibonacci(n: usize) -> bool {
+    fn is_perfect_square(x: usize) -> bool {
+        let s = (x as f64).sqrt() as usize;
+        s * s == x
+    }
+
+    is_perfect_square(5 * n * n + 4) || is_perfect_square(5 * n * n - 4)
+} 
+
 fn make_even_num_vec(vector: Vec<Vec<i32>>) -> Vec<(Vec<i32>, i8)>{
     let mut outputs: Vec<(Vec<i32>, i8)> = Vec::new();
     for (index, value) in vector.iter().enumerate() {
@@ -81,6 +91,18 @@ fn make_odd_num_vec(vector: Vec<Vec<i32>>) -> Vec<(Vec<i32>, i8)>{
     let mut outputs: Vec<(Vec<i32>, i8)> = Vec::new();
     for (index, value) in vector.iter().enumerate() {
         if index % 2 != 0 {
+            outputs.push((value.to_vec(), 1))
+        } else {
+            outputs.push((value.to_vec(), -1))
+        }
+    }
+    outputs
+}
+
+fn make_fibonacci_num_vec(vector: Vec<Vec<i32>>) -> Vec<(Vec<i32>, i8)> {
+    let mut outputs: Vec<(Vec<i32>, i8)> = Vec::new();
+    for (index, value) in vector.iter().enumerate() {
+        if is_fibonacci(index) {
             outputs.push((value.to_vec(), 1))
         } else {
             outputs.push((value.to_vec(), -1))
